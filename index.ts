75ff893ca8e56c68bc05dedb02d0fbc3bacdf63c
rice <pentappingminh@gmail.com>
2022-09-08 18:48:03 +0700

    add config port

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ const notFoundHandler = (req, res, next) => {
 };
 app.use(cors());
 // listen on port config.port
-app.listen(env.port || 3002, "0.0.0.0", () => {
+app.listen(process.env.PORT || 3000, () => {
   debug(`server started on port ${env.port} (${env.env})`);
 });
 
