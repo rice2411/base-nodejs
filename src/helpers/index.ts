fb10a83797dedb10328d3e3893cbc9d77f372817
rice <pentappingminh@gmail.com>
2022-09-09 17:42:04 +0700

    bug/wrong name function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,7 @@ export * from "./ApiError";
 export * from "./ApiResponse";
 export * from "./ApiResponseCode";
 export * from "./injector";
-export * from "./PasswordHash";
+export * from "./HashFunction";
 export * from "./Constant";
 export * from "./ApiResponseMessage";
 export * from "./injector";
