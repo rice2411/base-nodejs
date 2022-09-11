753e4851cc6a442cb4ec6bbf75e2ba6b9e6c20fd
rice <pentappingminh@gmail.com>
2022-09-12 00:08:45 +0700

    refactor code

--- src/routers/v1/auth.ts
+++ src/routers/v1/auth.ts
@@ -6,10 +6,6 @@ const router = express.Router();
 router.route("/login").post(authController.login);
 router.route("/register").post(authController.register);
 router.route("/verify").get(authController.verifyToken);
-router
-  .route("/verify-otp")
-  .get(authController.sendMailOTP)
-  .post(authController.verifyOTP);
 router.route("/reset-password").post(authController.resetPassword);
 
 export default router;
