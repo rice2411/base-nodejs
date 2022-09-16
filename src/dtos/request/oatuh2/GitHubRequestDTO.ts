dd47e6bf5a6931d1485b4a1f125e12a8d231c7ab
rice <pentappingminh@gmail.com>
2022-09-17 02:04:11 +0700

    bug github login

--- src/dtos/request/oatuh2/GitHubRequestDTO.ts
+++ src/dtos/request/oatuh2/GitHubRequestDTO.ts
@@ -12,11 +12,15 @@ export default class GitHubRequestDTO {
   public type_account?: string;
 
   constructor(data) {
+    if (data?.emails) {
+      this.email = data?.emails[0]?.value;
+    }
+    if (data?.photos) {
+      this.avatar = data?.photos[0]?.value;
+    }
     this.github_id = data.id;
     this.first_name = "";
     this.last_name = data.username;
-    this.email = data.emails[0].value;
-    this.avatar = data.photos[0].value;
     this.password = uuidv4();
     this.username = NAME_DEFAULT + this.github_id;
     this.type_account = ACCOUNT_TYPE.GITHUB;
